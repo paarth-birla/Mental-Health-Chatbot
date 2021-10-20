106d09e0215aa4b1145672728cc931e6d122dae5
Rishikesh2001 <rishikeshpashte223@gmail.com>
2021-10-20 11:40:53 +0530

    Added dashboard, settings and articles pages...

--- lib/pages/articles.dart
+++ lib/pages/articles.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Dashboard extends StatefulWidget {
-  const Dashboard({Key? key}) : super(key: key);
+class Articles extends StatefulWidget {
+  const Articles({Key? key}) : super(key: key);
 
   @override
-  _DashboardState createState() => _DashboardState();
+  _ArticlesState createState() => _ArticlesState();
 }
 
-class _DashboardState extends State<Dashboard> {
+class _ArticlesState extends State<Articles> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +17,7 @@ class _DashboardState extends State<Dashboard> {
         centerTitle: true,
         backgroundColor: Colors.blue,
         title: Text(
-          'Dashboard',
+          'Articles',
           style: GoogleFonts.scheherazade(
               fontSize: 46.0, fontWeight: FontWeight.w600),
         ),
