dfc15ad8424f1f43e1773431daf24cefb9b9ddd0
Rishikesh2001 <rishikeshpashte223@gmail.com>
2021-10-22 20:03:27 +0530

    Completed Articles page

--- lib/pages/articles.dart
+++ lib/pages/articles.dart
@@ -1,5 +1,7 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Articles extends StatefulWidget {
   const Articles({Key? key}) : super(key: key);
@@ -9,19 +11,95 @@ class Articles extends StatefulWidget {
 }
 
 class _ArticlesState extends State<Articles> {
+  TextEditingController _textController = TextEditingController();
+
+  static List<String> mainDataList = [
+    "Apple",
+    "Google",
+    "Facebook",
+    "Instagram",
+  ];
+
+  static Map links = {
+    "Apple": "https://www.apple.com/in/",
+    "Google": "https://www.google.co.in/",
+    "Facebook": "https://www.facebook.com/",
+    "Instagram": "https://www.instagram.com/",
+  };
+
+  // Copy Main List into New List.
+  List<String> newDataList = List.from(mainDataList);
+
+  onItemChanged(String value) {
+    setState(() {
+      newDataList = mainDataList
+          .where((string) => string.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
+  openUrl(String data) async {
+    if (await canLaunch(links[data])) {
+      await launch(links[data]);
+    } else {
+      throw 'Could not launch url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       // App bar of the page
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.blue,
         title: Text(
           'Articles',
-          style: GoogleFonts.scheherazade(
-              fontSize: 46.0, fontWeight: FontWeight.w600),
+          style: GoogleFonts.andada(
+            fontSize: 34.0,
+            fontWeight: FontWeight.w500,
+          ),
         ),
+      ),
+
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                hintText: 'Search Here...',
+                hintStyle: GoogleFonts.andada(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              onChanged: onItemChanged,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(10.0),
+              children: newDataList.map((data) {
+                return ListTile(
+                  title: Text(
+                    data,
+                    style: GoogleFonts.andada(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  onTap: () => openUrl(data),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+// CheckPoint
